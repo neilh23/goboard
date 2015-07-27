1586b91483987343a350e1adad7ac5831bcb43b3
Neil Howie <neil.howie@yahoo.co.uk>
2015-07-27 09:47:37 +0100

    Failing tests for branches

---
+++ test/unit/sgf-branch-test.js
@@ -0,0 +1,69 @@
+import GameModel from '../../src/gamemodel';
+import SGFParser from '../../src/sgfparser';
+
+/* eslint-disable no-unused-expressions,no-loop-func */
+
+describe('Branch tests', () => {
+  describe('Basic branch test', () => {
+    var model = new GameModel();
+
+    var parser = new SGFParser(model);
+    parser.parse('(;GM[1]FF[4]KM[5.5];B[pd];W[qf](;B[nc];W[rd];B[qh];W[qc];B[qe];W[re];B[pf])(;B[nd];W[qc];B[qd];W[rd];B[pc]))');
+    model.goToMove(1);
+
+    it(`should parse move 1 correctly`, () => {
+      expect(model.stoneAt(15, 15)).to.equal('b');
+      expect(model.currentMoveNumber()).to.equal(1);
+      expect(model.branch).to.equal(0); // main branch
+      expect(model.getBranches().length).to.equal(1);
+    });
+
+    it(`should parse move 3 correctly as branch 0`, () => {
+      model.goToMove(3);
+      expect(model.stoneAt(15, 3)).to.equal('b');
+      expect(model.stoneAt(16, 5)).to.equal('w');
+      expect(model.stoneAt(13, 2)).to.equal('b');
+      expect(model.currentMoveNumber()).to.equal(3);
+      expect(model.branch).to.equal(0); // main branch
+      expect(model.getBranches().length).to.equal(2);
+    });
+
+    it(`should maintain branch 0 correctly`, () => {
+      model.goToMove(7);
+      expect(model.stoneAt(15, 3)).to.equal('b');
+      expect(model.stoneAt(16, 5)).to.equal('w');
+      expect(model.stoneAt(13, 2)).to.equal('b');
+      expect(model.stoneAt(16, 2)).to.equal('w');
+      expect(model.stoneAt(15, 2)).to.equal('b');
+      expect(model.stoneAt(16, 3)).to.equal('w');
+
+      expect(model.currentMoveNumber()).to.equal(7);
+      expect(model.branch).to.equal(1); // alt branch
+      expect(model.getBranches().length).to.equal(2);
+    });
+
+    it(`should go to move 3 on branch 1 correctly`, () => {
+      model.goToMove(3, 1);
+      expect(model.stoneAt(15, 3)).to.equal('b');
+      expect(model.stoneAt(16, 5)).to.equal('w');
+      expect(model.stoneAt(13, 3)).to.equal('b');
+      expect(model.currentMoveNumber()).to.equal(3);
+      expect(model.branch).to.equal(1); // alt branch
+      expect(model.getBranches().length).to.equal(2);
+    });
+
+    it(`should maintain branch 1 correctly`, () => {
+      model.goToMove(9);
+      expect(model.stoneAt(15, 3)).to.equal('b');
+      expect(model.stoneAt(16, 5)).to.equal('w');
+      expect(model.stoneAt(13, 3)).to.equal('b');
+      expect(model.stoneAt(16, 2)).to.equal('w');
+      expect(model.stoneAt(13, 2)).to.equal('b');
+
+      expect(model.currentMoveNumber()).to.equal(9);
+      expect(model.branch).to.equal(1); // alt branch
+      expect(model.getBranches().length).to.equal(2);
+    });
+  });
+
+});
